24c4d9af370ccfc0c8a89df4e00149d3a6cdebb9
Diastowo <diastowo@gmail.com>
2017-06-18 21:22:59 +0700

    Add Readme.txt

---
+++ MyTrees/Protocol.swift
@@ -0,0 +1,9 @@
+//
+//  Protocol.swift
+//  MyTrees
+//
+//  Created by Eldien Hasmanto on 6/8/17.
+//  Copyright © 2017 Eldien Hasmanto. All rights reserved.
+//
+
+import Foundation
---
+++ MyTrees/StoreMe.swift
@@ -0,0 +1,9 @@
+//
+//  StoreMe.swift
+//  MyTrees
+//
+//  Created by Eldien Hasmanto on 6/13/17.
+//  Copyright © 2017 Eldien Hasmanto. All rights reserved.
+//
+
+import Foundation
